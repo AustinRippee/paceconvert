dbeb40f267fa70a7ced3ba981ff948ed3d9be250
Austin Rippee <austinrippee@gmail.com>
2025-04-24 19:50:43 -0400

    Added splitting times into the parser.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        double score;
+        double dblTimeMin = 0.0;
+        double dblTimeSec = 0.0;
+
         System.out.println("Hello to PaceConvert!");
 
         Scanner scRaceTime = new Scanner(System.in);
@@ -16,12 +20,33 @@ public class Main {
         System.out.println("Enter your race time:");
 
         String raceTime = scRaceTime.nextLine();
+        String[] totalTime = raceTime.split("[:]");
+
+        String timeMin = totalTime[0];
+        String timeSec = totalTime[1];
+        System.out.println("Minutes: " + timeMin);
+        System.out.println("Seconds: " + timeSec);
+
+        try {
+            dblTimeMin = Double.parseDouble(timeMin);
+            dblTimeSec = Double.parseDouble(timeSec);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid format");
+        }
 
         switch(raceDistance){
-            case "800m", "1 Mile", "2 Mile":
+            case "800m":
+                score = 302.9089*(800/((60*dblTimeMin)+dblTimeSec)) - 1377.5673;
+
                 System.out.println("Current Race");
                 System.out.println("Time: " + raceTime);
                 System.out.println("Distance: " + raceDistance);
+                System.out.println("Score: " + score);
+                break;
+            case "1 Mile":
+                break;
+            case "2 Mile":
                 break;
             case "5000m":
                 System.out.println("Hello");
