7fd1df47c4a6834aa10b4d85879fd6b9b49614a4
Austin Rippee <austinrippee@gmail.com>
2025-04-24 20:43:01 -0400

    Added ability to do less than 1 minute times.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,46 +7,63 @@ public class Main {
         double score;
         double dblTimeMin = 0.0;
         double dblTimeSec = 0.0;
+        String[] totalTime = {};
+        String timeMin = "";
+        String timeSec = "";
 
         System.out.println("Hello to PaceConvert!");
+        System.out.println("====================================");
 
-        Scanner scRaceTime = new Scanner(System.in);
-        Scanner scRaceDistance = new Scanner(System.in);
+        Scanner scanMain = new Scanner(System.in);
 
         System.out.println("Enter your race distance:");
         System.out.println("You can only choose from 800m, 1600m, or 3200m");
-
-        String raceDistance = scRaceDistance.nextLine();
+        String raceDistance = scanMain.nextLine();
 
         System.out.println("Enter your race time:");
+        String raceTime = scanMain.nextLine();
 
-        String raceTime = scRaceTime.nextLine();
-        String[] totalTime = raceTime.split("[:]");
+        if (raceTime.contains(":")) {
+            totalTime = raceTime.split(":");
+            timeMin = totalTime[0];
+            timeSec = totalTime[1];
+            try {
+                dblTimeMin = Double.parseDouble(timeMin);
+                dblTimeSec = Double.parseDouble(timeSec);
 
-        String timeMin = totalTime[0];
-        String timeSec = totalTime[1];
-        System.out.println("Minutes: " + timeMin);
-        System.out.println("Seconds: " + timeSec);
-
-        try {
-            dblTimeMin = Double.parseDouble(timeMin);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid format");
+            }
+        }
+        else {
+            timeSec = raceTime;
             dblTimeSec = Double.parseDouble(timeSec);
-
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid format");
         }
 
-        switch(raceDistance){
+        System.out.println(timeMin);
+        System.out.println(timeSec);
+
+
+        switch (raceDistance) {
+            case "400m":
+                if (dblTimeMin > 0) {
+                    System.out.println("Please enter 400m in seconds ONLY.");
+                    break;
+                } else {
+                    score = 262.37121 * (400 / dblTimeSec) - 1402.7708;
+                    System.out.println("Time: " + raceTime);
+                    System.out.println("Distance: " + raceDistance);
+                    System.out.println("Score: " + score);
+                    break;
+                }
             case "800m":
                 score = 302.9089*(800/((60*dblTimeMin)+dblTimeSec)) - 1377.5673;
-                System.out.println("Current Race");
                 System.out.println("Time: " + raceTime);
                 System.out.println("Distance: " + raceDistance);
                 System.out.println("Score: " + score);
                 break;
             case "1 Mile":
                 score = 321.7731201*(1609.34/((60*dblTimeMin)+dblTimeSec)) -1306.285127;
-                System.out.println("Current Race");
                 System.out.println("Time: " + raceTime);
                 System.out.println("Distance: " + raceDistance);
                 System.out.println("Score: " + score);
@@ -53,10 +71,18 @@ public class Main {
             case "2 Mile":
                 break;
             case "5000m":
-                System.out.println("Hello");
+                score = 342.8535*(5000/((60*dblTimeMin)+dblTimeSec)) - 1234.1959;
+                System.out.println("Time: " + raceTime);
+                System.out.println("Distance: " + raceDistance);
+                System.out.println("Score: " + score);
                 break;
             case "10000m":
                 System.out.println("What");
+                break;
+            default:
+                System.out.println("That is not a valid race type.");
+                System.out.println("Choose from the list of available race types:");
+                System.out.println("110m, 200, 300m, 400m, 800m, 1500, 1 Mile, 3000m, 2 Mile, 5000m, 10000m");
                 break;
         }
     }
